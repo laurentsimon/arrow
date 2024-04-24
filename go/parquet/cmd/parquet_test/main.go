702f3685c9b00e343b47324c40699a2c75ccca54
laurentsimon <laurentsimon@google.com>
2024-04-24 23:08:32 +0000

    update
    Signed-off-by: laurentsimon <laurentsimon@google.com>

--- go/parquet/cmd/parquet_test/main.go
+++ go/parquet/cmd/parquet_test/main.go
@@ -18,11 +18,14 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	//"github.com/apache/arrow/go/v17/parquet"
 	"github.com/apache/arrow/go/v17/parquet/cmd/parquet_test/buffer"
 	"github.com/apache/arrow/go/v17/parquet/file"
+	"github.com/apache/arrow/go/v17/parquet/metadata"
+	"github.com/apache/arrow/go/v17/parquet/schema"
 )
 
 func usage() {
@@ -45,6 +48,9 @@ func main() {
 		fmt.Fprintf(os.Stderr, "create buffer: %v", err)
 		os.Exit(1)
 	}
+	// NOTE: Can use WithReadProps for cache size and streaming.
+	// Also look at WithMetadata. If data is too large, may want to read from file
+	// from OpenParquetFile instead.
 	prd, err := file.NewParquetReader(buf)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "new parquest reader: %v", err)
@@ -54,4 +60,85 @@ func main() {
 	fmt.Println("Version:", prdMetadata.Version())
 	fmt.Println("Created By:", prdMetadata.GetCreatedBy())
 	fmt.Println("Num Rows:", prd.NumRows())
+
+	keyvaluemeta := prdMetadata.KeyValueMetadata()
+	if keyvaluemeta != nil {
+		fmt.Println("Key Value Metadata:", keyvaluemeta.Len(), "entries")
+		keys := keyvaluemeta.Keys()
+		values := keyvaluemeta.Values()
+		for i := 0; i < keyvaluemeta.Len(); i++ {
+			fmt.Printf(">> Key nr %d:\n%q: %q\n", i, keys[i], values[i])
+		}
+	}
+
+	fmt.Println("Number of RowGroups:", prd.NumRowGroups())
+	fmt.Println("Number of Real Columns:", prdMetadata.Schema.Root().NumFields())
+	fmt.Println("Number of Columns:", prdMetadata.Schema.NumColumns())
+	selectedColumns := []int{}
+	for i := 0; i < prdMetadata.Schema.NumColumns(); i++ {
+		selectedColumns = append(selectedColumns, i)
+	}
+	for _, c := range selectedColumns {
+		descr := prdMetadata.Schema.Column(c)
+		fmt.Printf("Column %d: %s (%s", c, descr.Path(), descr.PhysicalType())
+		if descr.ConvertedType() != schema.ConvertedTypes.None {
+			fmt.Printf("/%s", descr.ConvertedType())
+			if descr.ConvertedType() == schema.ConvertedTypes.Decimal {
+				dec := descr.LogicalType().(*schema.DecimalLogicalType)
+				fmt.Printf("(%d,%d)", dec.Precision(), dec.Scale())
+			}
+		}
+		fmt.Print(")\n")
+	}
+
+	for r := 0; r < prd.NumRowGroups(); r++ {
+		fmt.Println("--- Row Group:", r, " ---")
+
+		rgr := prd.RowGroup(r)
+		rowGroupMeta := rgr.MetaData()
+		fmt.Println("--- Total Bytes:", rowGroupMeta.TotalByteSize(), " ---")
+		fmt.Println("--- Rows:", rgr.NumRows(), " ---")
+
+		for _, c := range selectedColumns {
+			chunkMeta, err := rowGroupMeta.ColumnChunk(c)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println("Column", c)
+			if set, _ := chunkMeta.StatsSet(); set {
+				stats, err := chunkMeta.Statistics()
+				if err != nil {
+					log.Fatal(err)
+				}
+				fmt.Printf(" Values: %d", chunkMeta.NumValues())
+				if stats.HasMinMax() {
+					fmt.Printf(", Min: %v, Max: %v",
+						metadata.GetStatValue(stats.Type(), stats.EncodeMin()),
+						metadata.GetStatValue(stats.Type(), stats.EncodeMax()))
+				}
+				if stats.HasNullCount() {
+					fmt.Printf(", Null Values: %d", stats.NullCount())
+				}
+				if stats.HasDistinctCount() {
+					fmt.Printf(", Distinct Values: %d", stats.DistinctCount())
+				}
+				fmt.Println()
+			} else {
+				fmt.Println(" Values:", chunkMeta.NumValues(), "Statistics Not Set")
+			}
+
+			fmt.Print(" Compression: ", chunkMeta.Compression())
+			fmt.Print(", Encodings:")
+			for _, enc := range chunkMeta.Encodings() {
+				fmt.Print(" ", enc)
+			}
+			fmt.Println()
+			fmt.Print(" Uncompressed Size: ", chunkMeta.TotalUncompressedSize())
+			fmt.Println(", Compressed Size:", chunkMeta.TotalCompressedSize())
+		}
+	}
+
+	fmt.Println("--- Values ---")
+
 }
